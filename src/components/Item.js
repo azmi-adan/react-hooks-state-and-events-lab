cfe72b0431225d4ee2025c721cc817b3add0b43e
azmi adan <azmi.adan2032@gmail.com>
2024-08-04 23:51:21 +0300

    add all

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
+  const handleCartToggle = () => {
+    setIsInCart(!isInCart);
+  };
+  // / Determine the button text based on the item's cart status
+  const buttonText = isInCart ? "Remove From Cart" : "Add to Cart";
+
+  // Determine the li class name based on the item's cart status
+  const liClassName = isInCart ? "in-cart" : "";
+  
+ 
   return (
-    <li className="">
+    <li className={liClassName}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCartToggle}>Add to Cart {buttonText}</button>
     </li>
   );
 }
